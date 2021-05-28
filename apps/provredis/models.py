861b407fcd7cdac2e7cfb0d6410a58e1df29167b
chenzw <1546960408@qq.com>
2021-05-28 17:50:03 +0800

    增加序列化器功能

--- apps/provredis/models.py
+++ apps/provredis/models.py
@@ -1,7 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class ProvredisDetail(models.Model):
+    passid = models.CharField(verbose_name='车辆通行id', max_length=40, primary_key=True, help_text='车辆通行id')
+    id = models.CharField(verbose_name='交易流水id', max_length=500, help_text='交易流水id')
+    serprovinceid = models.CharField(verbose_name='通行方id', max_length=500, help_text='通行方id')
+    issuerid = models.CharField(verbose_name='发行方id', max_length=500, help_text='发行方id')
+    mediatype = models.CharField(verbose_name='介质类型', max_length=500, help_text='介质类型')
+    exitfeetype = models.CharField(verbose_name='计费类型', max_length=500, help_text='计费类型')
+    datasource = models.CharField(verbose_name='交易流水类型', max_length=500, help_text='交易流水类型')
+    extime = models.CharField(verbose_name='出口交易时间', max_length=500, help_text='出口交易时间')
+    payfee = models.CharField(verbose_name='应收金额', max_length=500, help_text='应收金额')
+    fee = models.CharField(verbose_name='实收金额', max_length=500, help_text='实收金额')
+    discountfee = models.CharField(verbose_name='优惠金额', max_length=500, help_text='优惠金额')
+    sumtollfee = models.CharField(verbose_name='实收金额总和', max_length=500, help_text='实收金额总和')
+    uploadstatus = models.CharField(verbose_name='上传状态', max_length=500, help_text='上传状态')
+    expassstatus = models.CharField(verbose_name='交易流水状态', max_length=500, help_text='交易流水状态')
+    gantrypassdata = models.CharField(verbose_name='门架交易记录信息', max_length=500, help_text='门架交易记录信息')
+    obuprovfeesumafter = models.CharField(verbose_name='obu省内交易后金额', max_length=500, help_text='obu省内交易后金额')
+
+    class Meta:
+        db_table = 'tb_redis_main'
+        verbose_name = '行程组合主表'
+        verbose_name_plural = verbose_name
 
 
 class Provredis(models.Model):
-    name = models.CharField(max_length=500)
+    id = models.CharField(verbose_name='门架校验记录id', max_length=500,primary_key=True, help_text='门架校验记录id')
+    passid = models.CharField(verbose_name='车辆通行id', max_length=40, help_text='车辆通行id')
+    gantrytype = models.CharField(verbose_name='门架类型', max_length=500, help_text='门架类型')
+    type = models.CharField(verbose_name='门架介质类型', max_length=500, help_text='门架介质类型')
+    transtime = models.CharField(verbose_name='交易时间', max_length=500, help_text='交易时间')
+    payfee = models.CharField(verbose_name='应收金额', max_length=500, help_text='应收金额')
+    fee = models.CharField(verbose_name='实收金额', max_length=500, help_text='实收金额')
+    discountfee = models.CharField(verbose_name='优惠金额', max_length=500, help_text='优惠金额')
+    specialtype = models.CharField(verbose_name='特情类型', max_length=500, help_text='特情类型')
+
+    class Meta:
+        db_table = 'tb_redis_detail'
+        verbose_name = '行程组合子表'
+        verbose_name_plural = verbose_name
+
+
+
+
+
--- apps/provredis/views.py
+++ apps/provredis/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.shortcuts import render
-from django_cluster_redis.cache import ClusterRedis
 from django_redis import get_redis_connection
+from .serializers import ProvRedisSerializer
 
 conn = get_redis_connection()
 
@@ -10,6 +10,7 @@ conn = get_redis_connection()
 class PyRedis(View):
 
     # def get(self, request):
+    #
     #     data = [{"project": "项目1",
     #              "test": "czw1",
     #              "msg": "111"},
@@ -24,11 +25,11 @@ class PyRedis(View):
 
     def get(self, request):
         # data = conn.get('{014401204022700008915720210506233530}OBU')
-        redis_gantrypassdata_val = ["ID", "PASSID", "GANTRYTYPE", "TYPE", "TRANSTIME",
-                                         "PAYFEE", "FEE", "DISCOUNTFEE", "SPECIALTYPE"]
-        redis_main_val = ["ID", "SERPROVINCEID", "ISSUERID", "MEDIATYPE", "EXITFEETYPE", "DATASOURCE", "EXTIME",
-                               "PAYFEE", "FEE", "DISCOUNTFEE", "SUMTOLLFEE", "UPLOADSTATUS", "EXPASSSTATUS",
-                               "GANTRYPASSDATA", 'OBUPROVFEESUMAFTER']
+        redis_gantrypassdata_val = ["id", "passid", "gantrytype", "type", "transtime",
+                                    "payfee", "fee", "discountfee", "specialtype"]
+        redis_main_val = ["id", "serprovinceid", "issuerid", "mediatype", "exitfeetype", "datasource", "extime",
+                          "payfee", "fee", "discountfee", "sumtollfee", "uploadstatus", "expassstatus",
+                          "gantrypassdata", 'obuprovfeesumafter']
         try:
             redis_msg = conn.get('{014401204022700008915720210506233530}OBU').decode().split(",")
         except AttributeError:
@@ -39,4 +40,7 @@ class PyRedis(View):
             redis_msg[-2] = "null" if not gantry_li else sorted(gantry_li, key=lambda x: (x["TRANSTIME"]))
             # 对gantrypassdata按transtime升序排列
             data = dict(zip(redis_main_val, redis_msg))
-        return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False})
+            redis_obj = ProvRedisSerializer(instance=data)
+        # return JsonResponse(redis_obj.data, safe=False, json_dumps_params={'ensure_ascii': False})
+        return JsonResponse(redis_obj.data, json_dumps_params={'ensure_ascii': False})
+
